23cdaab3d5897a83ff4af40f338bb389a3c3ff9d
Isaac <iabella.4576@gmail.com>
2024-12-05 16:16:41 -0500

    Cleaning up directories, adding in old number generator from like project 1 or 2 I think

---
+++ random_num/numgen.cpp
@@ -0,0 +1,18 @@
+// random number generation
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+int main(){
+	std::srand(static_cast<unsigned int>(std::time(NULL)));
+
+	for(int i = 0; i <= 10000; ++i){
+	
+		std::cout << (std::rand() % 1000) + 1 << '\t';
+		if(i % 1 == 0){
+			std::cout << '\n';
+		}	
+	}
+	return 0;
+}
